48cc3ec16952f562612ea493fd96205ff00fde50
Rendradnta <165056185+Rendradnta@users.noreply.github.com>
2025-08-31 17:56:30 +0700

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster, TooltipProvider } from "@/components/ui";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
